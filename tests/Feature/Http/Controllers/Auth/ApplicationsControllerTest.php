a09420f80fc1768edc5ad5262bfbb43f72c670a7
Bola Bally <bolabally@gmail.com>
2020-03-28 10:29:23 +0000

    addApplicationsControllerTest

---
+++ tests/Feature/Http/Controllers/Auth/ApplicationsControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Auth;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ApplicationsControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function index_returns_a_view()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('home'));
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function index_redirects_when_unauthorized()
+    {
+        $response = $this->get(route('home'));
+
+        $response->assertStatus(302);
+    }
+}
