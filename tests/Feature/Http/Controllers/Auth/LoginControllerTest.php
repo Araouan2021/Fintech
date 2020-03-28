5348362aa9e4218843f4a612ecbfc180629e55b7
Bola Bally <bolabally@gmail.com>
2020-03-28 10:20:30 +0000

    addLoginControllerTest

--- tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers\Auth;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -18,6 +19,29 @@ class LoginControllerTest extends TestCase
 
     $response->assertStatus(200);
     $response->assertViewIs('auth.login');
+    }
+
+    /** @test */
+    public function login_displays_validation_errors()
+    {
+        $response = $this->post('/login', []);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors('email');
+    }
+
+    /** @test */
+    public function login_authenticates_and_redirects_user()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->post(route('login'), [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $response->assertRedirect(route('home'));
+        $this->assertAuthenticatedAs($user);
     }
 }
 
