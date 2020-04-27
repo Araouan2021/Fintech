e8fcd4e6022a90e7eb28ece4b830597968945ed2
Bola Bally <bolabally@gmail.com>
2020-04-27 09:49:23 +0000

    addfilesystems.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    
-    //Return view to upload file 
-    public function uploadFile(){
-        return view('uploadfile');
-    }
 
     /** Example of File Upload */
     public function uploadFileImage(Request $request){
